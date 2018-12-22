6bd9fd6b499fea94aeff36f6d260418e7fd80dd4
David Diederich <divido@psu.edu>
2018-12-22 14:54:43 -0500

    Updated inventory script to use the new backend model

--- inventory.py
+++ inventory.py
@@ -1,56 +1,32 @@
 #!/usr/bin/env python3
 
+# This script displays a list of all owned labels, and how many bottles of each
+# are currently in the cellar. Bottle lists are categorized by hold year.
+
+# --------------------------------------------------------------------------------
+
 from colorama import Fore, Back, Style
 from datetime import date
 
-import sqlite3
+from backend.cellar import Cellar
 
-db = sqlite3.connect('cellar.db')
-sql = db.cursor()
+cellar = Cellar()
 
-labelData = sql.execute("SELECT label.id, label.vintage, winery.name, label.name, region.name, region.country FROM label " +
-                        "LEFT JOIN winery ON label.winery = winery.id " +
-                        "LEFT JOIN region ON winery.region = region.id " +
-                        "ORDER BY winery.name, label.name, label.vintage").fetchall()
-
-labels = []
-for label in labelData:
-	blend = sql.execute("SELECT varietal.name, varietal.boldness, portion FROM blends " +
-	                    "LEFT JOIN varietal ON blends.varietal = varietal.id " +
-	                    "WHERE blends.label = ? " +
-	                    "ORDER BY portion DESC", (label[0],)).fetchall()
-
-	bottleCount = sql.execute("SELECT hold_until, COUNT(id) FROM bottle " +
-	                          "WHERE label = ? AND consumption IS NULL " +
-	                          "GROUP BY hold_until " +
-	                          "ORDER BY hold_until", (label[0],)).fetchall();
-
-	weightedBoldness = sum([b[1] * b[2] / 100.0 for b in blend])
-
-	if len(blend) == 1:
-		varietalDescription = blend[0][0]
-	else:
-		varietalDescription = ('(' + ', '.join(["%d%% %s" % (b[2], b[0]) for b in sorted(blend, key=lambda bl: bl[2], reverse=True)]) + ')')
-
-	if len(bottleCount) > 0:
-		labels.append({
-			'labelDescription': str(label[1]) + ' ' + label[2] + ' ' + label[3],
-			'regionDescription': label[4] + ', ' + label[5],
-			'varietalDescription': varietalDescription,
-			'boldness': weightedBoldness,
-			'bottleCount': bottleCount
-		})
+def stylize(fmt, value):
+	return fmt + value + Style.RESET_ALL
 
 currentYear = date.today().year
-for label in sorted(labels, key=lambda l: l['boldness'], reverse=True):
+for label in sorted(cellar.labels, key=lambda l: l.weightedBoldness, reverse=True):
 	print("%s  %s  %s" % (
-		(Style.BRIGHT + label['labelDescription'] + Style.RESET_ALL),
-		(Fore.BLUE + label['varietalDescription'] + Style.RESET_ALL),
-		(Style.DIM + label['regionDescription'] + Style.RESET_ALL)))
+		stylize(Style.BRIGHT, label.description),
+		stylize(Fore.BLUE, label.varietalDescription),
+		stylize(Style.DIM, label.winery.region.description)))
 
-	print(Fore.GREEN, end='')
-	for holdYear, holdAmt in label['bottleCount']:
-		print('  %d bottle%s to %s' % (
+	inventoryByYear = label.inventoryByYear()
+	for holdYear in sorted(inventoryByYear):
+		holdAmt = inventoryByYear[holdYear]
+		print(stylize(Fore.GREEN, '  %d bottle%s to %s' % (
 			holdAmt, '' if holdAmt == 1 else 's',
-			'drink now' if holdYear == currentYear else ('hold until %d' % holdYear)))
-	print(Style.RESET_ALL)
+			'drink now' if holdYear == currentYear else ('hold until %d' % holdYear))))
+
+	print()
