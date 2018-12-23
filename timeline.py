e52ae54ffcb09160207b6d117233757efd2bbb48
David Diederich <divido@psu.edu>
2018-12-22 22:33:56 -0500

    Updated timeline to utilize the new Cellar formats

--- timeline.py
+++ timeline.py
@@ -1,72 +1,40 @@
 #!/usr/bin/env python3
 
+# This script displays the number of bottles held for each year, and how much
+# excess or shortfall there will be in that year.
+
+# --------------------------------------------------------------------------------
 from colorama import Fore, Back, Style
-from datetime import date
-from dateutil.parser import parse
+from backend.cellar import Cellar
 
-import sqlite3
+cellar = Cellar()
 
-db = sqlite3.connect('cellar.db')
-sql = db.cursor()
+def stylize(fmt, value):
+	return fmt + value + Style.RESET_ALL
 
-bottleData = sql.execute("SELECT COUNT(consumption), MIN(consumption), MAX(consumption) FROM bottle " +
-                         "WHERE consumption IS NOT NULL").fetchone()
+# --------------------
 
-numYears = (parse(bottleData[2]) - parse(bottleData[1])).days / 365;
-bottlesPerYear = round(bottleData[0] / numYears)
+projection = cellar.consumptionProjection();
 
-# ----------------------------------------
+def showField(name, amt):
+	print('%s: %s' % (
+		stylize(Style.BRIGHT, name),
+		stylize(Fore.BLUE, '%d bottle%s' % (amt, '' if amt == 1 else 's'))))
 
-labelData = sql.execute("SELECT label.id FROM label").fetchall()
-countByYear = {}
+showField('Average Annual Consumption', projection['averageAnnualConsumption'])
+showField('Total Bottle Count', projection['totalBottleCount'])
+print()
 
-totalBottles = 0
-currentYear = date.today().year
-maxYear = currentYear
-labels = []
-for label in labelData:
-	bottleCount = sql.execute("SELECT hold_until, COUNT(id) FROM bottle " +
-	                          "WHERE label = ? AND consumption IS NULL " +
-	                          "GROUP BY hold_until", (label[0],)).fetchall()
+for holdYear in projection['byYear']:
+	count = projection['byYear'][holdYear]['count']
+	excess = projection['byYear'][holdYear]['excess']
 
-	for holdYear, holdAmt in bottleCount:
-		if holdYear <= currentYear:
-			holdYear = currentYear
+	if excess >= 0:
+		excessText = stylize(Fore.BLUE, '%2d Extra' % excess)
+	else:
+		excessText = '%2d Short' % -excess
 
-		maxYear = max(maxYear, holdYear)
-
-		if holdYear not in countByYear:
-			countByYear[holdYear] = 0
-		countByYear[holdYear] += holdAmt
-
-		totalBottles += holdAmt
-
-# ----------------------------------------
-
-carryover = 0
-print('%sAverage Annual Consumption%s: %s%d bottles%s' % (
-	Style.BRIGHT, Style.RESET_ALL,
-	Fore.BLUE, bottlesPerYear, Style.RESET_ALL))
-
-print('%sTotal Bottle Count%s: %s%d bottles%s\n' % (
-	Style.BRIGHT, Style.RESET_ALL,
-	Fore.BLUE, totalBottles, Style.RESET_ALL))
-
-for holdYear in range(currentYear, maxYear + 1):
-	holdAmt = countByYear[holdYear] if holdYear in countByYear else 0
-	expectedConsumption = bottlesPerYear
-
-	if holdYear == currentYear:
-		daysLeft = (date(currentYear + 1, 1, 1) - date.today()).days
-		expectedConsumption = round(bottlesPerYear * daysLeft / 365)
-
-	excessBottles = (holdAmt + carryover) - expectedConsumption
-	carryover = max(excessBottles, 0)
-
-	excessStyle = Fore.BLUE if excessBottles >= 0 else ''
-	excessText = 'Extra' if excessBottles >= 0 else 'Short'
-
-	print('%s%d%s: %s%2d bottle%s%s (%s%2d %s%s)' % (
-		Style.BRIGHT, holdYear, Style.RESET_ALL,
-		Fore.GREEN, holdAmt, ' ' if holdAmt == 1 else 's', Style.RESET_ALL,
-		excessStyle, abs(excessBottles), excessText, Style.RESET_ALL))
+	print('%s: %s (%s)' % (
+		stylize(Style.BRIGHT, str(holdYear)),
+		stylize(Fore.GREEN, '%2d bottle%s' % (count, ' ' if count == 1 else 's')),
+		excessText))
